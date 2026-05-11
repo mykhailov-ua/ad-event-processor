df85e330003643dfb296cc2387a6ee3b0ff13753
Yaroslav Mykhailov <mykhailov.yar@gmail.com>
2026-05-11 10:55:08 +0300

    feat(auth): introduce atomic store for session management

---
+++ internal/auth/repository/store.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Store interface {
+	Querier
+	ExecTx(ctx context.Context, fn func(Querier) error) error
+}
+
+type SQLStore struct {
+	*Queries
+	db *pgxpool.Pool
+}
+
+func NewStore(db *pgxpool.Pool) Store {
+	return &SQLStore{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (s *SQLStore) ExecTx(ctx context.Context, fn func(Querier) error) error {
+	tx, err := s.db.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(ctx); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit(ctx)
+}
